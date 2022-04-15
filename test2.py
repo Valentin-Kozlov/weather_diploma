4fa8148663394547a0b0ec7dd8785cda9efcdb52
Valentin-Kozlov <proxi10@yandex.ru>
2022-04-15 17:28:27 +0500

    update test2.py

--- test2.py
+++ test2.py
@@ -4,7 +4,19 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI']= "mysql://root:root@192.168.146.162:3306/test"
+app.config['SQLALCHEMY_DATABASE_URI']= "mysql+mysqldb://root:root@192.168.146.162:3306/test_flask"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+
+class Date_table(db.Model):
+    id = db.Column(db.Integer, primary_key=True) 
+    weather_state_name = db.Column(db.String(20), nullable=True)
+    wind_direction_compass = db.Column(db.String(20), nullable=True) 
+    created = db.Column(db.String(40), nullable=True) 
+    applicable_date = db.Column(db.DateTime(20), nullable=False)
+    min_temp = db.Column(db.String(20), nullable=True) 
+    max_temp = db.Column(db.String(20), nullable=True) 
+    the_temp = db.Column(db.String(20), nullable=True)
+    
+db.create_all()
